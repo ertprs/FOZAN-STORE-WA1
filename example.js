d2d29d709e895c49b382dcc876c90b596933200b
Glitch (accurate-mangrove-operation) <none>
2020-12-30 14:46:47 +0000

    🏉❣️ Checkpoint ./example.js:23794624/844 ./package.json:23794624/5756

--- example.js
+++ example.js
@@ -1,6 +1,9 @@
+
+const urls = ''
 const fs = require("fs");
 const { Client, MessageMedia } = require("whatsapp-web.js");
 const express = require("express");
+const fetch = require("node-fetch");
 const app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -51,6 +54,25 @@ app.use(bodyParser.json({ limit: "50mb" })); // for parsing application/json
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true })); // for parsing       application/x-www-form-urlencoded
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+//fetch url
+
+const saveData = async function(data) {
+  return await fetch(urls, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    // credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json"
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  })
+};
+//
 
 io.on("connection", async socket => {
   console.log(io.engine.clientsCount + " client connected");
