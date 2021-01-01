d77e57b5c9c0508a6a62e7df120a84405106c146
Glitch (accurate-mangrove-operation) <none>
2021-01-01 11:12:30 +0000

    📯🏣 Checkpoint ./example.js:23794624/11

--- example.js
+++ example.js
@@ -31,8 +31,8 @@ const client = new Client({
   restartOnAuthFail: true,
   // userAgent:
   //   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
-  authTimeoutMs: 95000,
-  takeoverOnConflict: true,
+  authTimeoutMs: 45000,
+  takeoverOnConflict: false,
   takeoverTimeoutMs: 60000,
   puppeteer: {
     headless: true,
