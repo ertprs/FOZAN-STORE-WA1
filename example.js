9b6ab88edf8940f80ace478534812a4bb0f214b2
Glitch (accurate-mangrove-operation) <none>
2021-01-01 09:49:30 +0000

    🐿👩 Checkpoint ./example.js:23794624/14

--- example.js
+++ example.js
@@ -29,8 +29,8 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
 let qrCode;
 const client = new Client({
   restartOnAuthFail: true,
-  userAgent:
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
+  // userAgent:
+  //   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
   authTimeoutMs: 95000,
   takeoverOnConflict: true,
   takeoverTimeoutMs: 60000,
